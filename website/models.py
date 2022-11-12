0a957eed6e30d9e1434684daa71dce31a66c0024
Ryan Tran <73788695+RyanBoomer30@users.noreply.github.com>
2022-11-12 12:30:33 -0500

    Add files via upload

--- website/models.py
+++ website/models.py
@@ -5,3 +5,11 @@ from ckeditor.fields import RichTextField
 
 class User(AbstractUser):
     pass
+
+class Member(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name="user")
+    name = models.CharField(max_length=30)
+    admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} is an admin: {self.admin}"
--- website/views.py
+++ website/views.py
@@ -7,10 +7,65 @@ from django.core.paginator import Paginator
 from django import forms
 from django.views.generic import CreateView
 from django.forms import ModelForm
-from ckeditor.widgets import CKEditorWidget
 import json
 
 from .models import *
 
 def index(request):
-    return HttpResponse("website")
+    return render(request, "website/index.html")
+
+# Login
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("member"))
+        else:
+            return render(request, "website/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "website/login.html")
+
+# Logout
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+# Register
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        name = request.POST["name"]
+        position = request.POST["position"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "website/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            member = Member.objects.create(user=user, name=name, position=position)
+            member.save()
+        except IntegrityError:
+            return render(request, "website/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "website/register.html")
