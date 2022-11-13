b53a8bb43494c0ba7db27a73012de856f3965996
Ryan Tran <73788695+RyanBoomer30@users.noreply.github.com>
2022-11-13 09:19:01 -0500

    final commit

--- emotions.py
+++ emotions.py
@@ -5,4 +5,4 @@ def getemotions(str):
     data = text_object.raw_emotion_scores
     return data
 
-print(getemotions("Today is a really dark and rainy day"))
+print(getemotions("The times are difficult! Our sales have been disappointing for the past three quarters for our data analytics product suite. We have a competitive data analytics product suite in the industry. However, we are not doing a good job at selling it, and this is really frustrating."))
--- network/views.py
+++ network/views.py
@@ -16,33 +16,65 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-import io
 import random
 from datetime import datetime
 import string
 from django.conf import settings
-import os
+from PIL import Image, ImageDraw
+from collections import Counter 
 
 def drawgraph(result):
-    plt.clf()
+    emotions = {
+        'fear': [[172,154,186], [120,100,152], [123,63,165], [101,70,122], [72,62,84]], 
+        'anger': [[231,34,34], [201,41,41], [184,47,47], [171,50,50], [150,50,50]], 
+        'anticipation': [[241, 222, 193], [205, 182, 153], [105, 135, 121], [214, 200, 158], [252, 243, 197]], 
+        'trust': [[253,147,174], [111,225,225], [246,197,99], [223,150,75], [164,75,0]], 
+        'surprise': [[255, 255, 255], [255, 248, 232], [252, 213, 129], [213, 41, 65], [153, 13, 53]], 
+        'positive': [[253, 253, 189], [200, 255, 212], [184, 232, 252], [77, 175, 255], [240, 255, 66]], 
+        'negative': [[101,151,197], [147,191,69], [152,130,37], [139,66,25], [45,11,0]], 
+        'sadness': [[36,36,36], [84,68,68], [118,118,97], [129,146,121], [193,190,132]], 
+        'disgust': [[225,145,24], [195,120,43], [141,92,17], [212,131,29], [229,155,14]], 
+        'joy': [[230,27,27], [247,244,241], [243,229,10], [10,113,243], [199,34,224]]
+    }
 
-    emotions = {'fear': 'm', 'anger': 'r', 'anticipation': 'm', 'trust': 'g', 'surprise': 'y', 'positive': 'y', 'negative': 'r', 'sadness': 'b', 'disgust': 'g', 'joy': 'y'}
-    x = []
-    y = []
-    colors = []
+    highestEmotion = max(result, key=result.get)
 
-    for key in result:
-        x += [key]
-        y += [result[key]]
-        colors += emotions[key]
+    colors = emotions[highestEmotion]
+
+    print(colors)
         
     figure = ''.join(random.choices(string.ascii_lowercase, k=32))
-    plt.bar(x,y, color=colors, alpha=0.65)
 
-    plt.xticks([])
-    plt.yticks([])
+    image = Image.new('RGB', (2000, 2000))
+    width, height = image.size
 
-    plt.savefig("static/images/" + figure + ".png")
+    rectangle_width = 100
+    rectangle_height = 100
+
+    number_of_squares = 10000
+
+    draw_image = ImageDraw.Draw(image)
+
+    for i in range(number_of_squares):
+        rectangle_x = random.randint(0, width)
+        rectangle_y = random.randint(0, height)
+
+        rectangle_shape = [
+            (rectangle_x, rectangle_y),
+            (rectangle_x + rectangle_width, rectangle_y + rectangle_height)]
+
+        j = random.randint(0, 4)
+        draw_image.rectangle(
+            rectangle_shape,
+            fill=(
+                colors[j][0],
+                colors[j][1],
+                colors[j][2]
+            )
+        )
+
+    # plt.savefig("static/images/" + figure + ".png")
+    image.save("static/images/" + figure + ".png")
 
     return (figure + ".png")
 
@@ -60,21 +92,27 @@ class CreatePost(forms.Form):
 def index(request):
     user = request.user
     if (user.is_authenticated):
-        all_posts = Post.objects.filter(owner=user).order_by('-time')
-        if (all_posts.first().time == timezone.now):
-            status = True
+        if Post.objects.filter(owner=user).exists():
+            all_posts = Post.objects.filter(owner=user).order_by('-time')
+            if (all_posts.first().time == timezone.now):
+                status = False
+            else:
+                status = True
+            
+            # Render posts in order of 10 per paginator
+            paginator = Paginator(all_posts, 10)
+            page_number = request.GET.get('page')
+            posts = paginator.get_page(page_number)
+            return render(request, "network/index.html", {
+                "form": CreatePost(),
+                'posts': posts,
+                'status': status
+            })
         else:
-            status = False
-        
-        # Render posts in order of 10 per paginator
-        paginator = Paginator(all_posts, 10)
-        page_number = request.GET.get('page')
-        posts = paginator.get_page(page_number)
-        return render(request, "network/index.html", {
-            "form": CreatePost(),
-            'posts': posts,
-            'status': status
-        })
+            return render(request, "network/index.html", {
+                "form": CreatePost(),
+                'status': True
+            })
     else:
         return render(request, "network/login.html")
 
@@ -165,7 +203,11 @@ def edit(request, post_id, edit):
     })
 
 def calendar(request):
-    return render(request, "network/calendar.html")
+    user = request.user
+    all_posts = Post.objects.filter(owner=user).order_by('time')
+    return render(request, "network/calendar.html",{
+        "posts": all_posts
+    })
 
 def resources(request):
     return render(request, "network/healthresources.html")
